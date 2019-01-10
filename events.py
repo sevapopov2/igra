c5be0dab6638c06c9353ae95782d5ba35f15b60d
Seva <sevapopov13@gmail.com>
2019-01-10 23:39:31 +0300

    Добавления нового случайного события
    Добавление нового случайного события, не запускается программа, не могу выяснить почему. Событие, предполагается, что ÷герою за активность выдается какое-то количество денег в неделю, планирую сделать, чтобы за отдых деньги не выдавались. Пока денег будет выдаваться случайное количество.

--- events.py
+++ events.py
@@ -4,6 +4,11 @@ class randomevent:
     def instrumentbreak():
         if random.random() > 0.9:
             return True
-
         else:
             return False
+    def gettingmoney():
+        if random.random >= 0.5:
+            return True
+        else:
+            return False
+#Идея заключается в том, что ученику могут давать деньги за его активность, репитицию / учебу, которые он может тратить в том числе на починку сломанного инструмента.
